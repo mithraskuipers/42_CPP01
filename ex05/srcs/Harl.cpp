ca24dcdbfde707ed8019b85bacdfdc524be6584f
Mithras Kuipers <mithraskuipers@gmail.com>
2022-10-21 21:43:25 +0200

    added ex05

--- ex05/srcs/Harl.cpp
+++ ex05/srcs/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: mkuipers <mkuipers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/21 15:57:10 by mkuipers      #+#    #+#                 */
-/*   Updated: 2022/10/21 21:38:21 by mkuipers      ########   odam.nl         */
+/*   Updated: 2022/10/21 21:43:07 by mkuipers      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,22 +25,22 @@ Harl::~Harl()
 
 void	Harl::debug(void)
 {
-	std::cout << "" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cout << "" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << "" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout << ""	<< std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 int	getLevel(std::string level)
